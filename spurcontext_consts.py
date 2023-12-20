d1b3d62e5d4d178b4ee7a3e04b48b6d05d5ca13b
hardik-crest <hchavda@splunk.com>
2023-12-20 17:59:12 +0530

    updated copyright and logos

--- spurcontext_consts.py
+++ spurcontext_consts.py
@@ -1,6 +1,6 @@
 # File: spurcontext_consts.py
 #
-# Copyright (c) Community, 2023
+# Copyright (c) 2023 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
